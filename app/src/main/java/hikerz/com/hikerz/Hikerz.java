cc2f90e475be5c184a4d18979d96230f907a1786
hborah <hemangakrishnaborah@gmail.com>
2016-08-27 21:03:02 -0700

    Working app bar

--- app/src/main/java/hikerz/com/hikerz/Hikerz.java
+++ app/src/main/java/hikerz/com/hikerz/Hikerz.java
@@ -1,23 +1,17 @@
 package hikerz.com.hikerz;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.TabWidget;
 import android.widget.TextView;
 
 public class Hikerz extends AppCompatActivity {
@@ -58,7 +52,7 @@ public class Hikerz extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hikerz, menu);
+        //getMenuInflater().inflate(R.menu.menu_hikerz, menu);
         return true;
     }
 

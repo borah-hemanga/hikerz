d545f9a70ea9e9b44027df71300aa4697811298e
hborah <hemangakrishnaborah@gmail.com>
2016-08-28 03:33:16 -0700

    Added files

---
+++ app/src/main/java/utils/PrimaryConstants.java
@@ -0,0 +1,9 @@
+package utils;
+
+/**
+ * Created by hborah on 8/28/16.
+ */
+public class PrimaryConstants {
+    static final String MY_IMAGE = "my_img";
+    static final String IMAGE_STORE_DIR = "HIKERZ_IMAGES";
+}
